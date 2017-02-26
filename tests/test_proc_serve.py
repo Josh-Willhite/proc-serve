7c39aefcdba824ddf82eebcf0c651227040cecfb
josh willhite <jwillhite@gmail.com>
2017-02-26 13:57:16 -0800

    phase-1: Test parsing against known good output.

--- tests/test_proc_serve.py
+++ tests/test_proc_serve.py
@@ -1,18 +1,16 @@
 import sys
+import json
 sys.path.append('../src')
 
-from proc_serve import cpu_info, mem_info, uptime
-
-def test_cpu_info():
-    pass
+from proc_serve import get_info, proc_to_dict
 
 
-def test_mem_info():
-    assert 1==1
-    pass
+def test_proc_to_dict():
+    for proc_file in ['cpuinfo', 'meminfo']:
+        info_raw = get_info(proc_file)
+        info = proc_to_dict(info_raw)
 
+        with open('./{}.json'.format(proc_file), 'r') as f:
+            expected = json.load(f)
 
-def test_uptime():
-    assert 1==1
-    pass
-
+        assert len(set(info.items()) & set(expected.items())) == len(expected)
