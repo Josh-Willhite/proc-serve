83dd24a0cf75374dd1a2a2001fc61d82946cf371
josh willhite <jwillhite@gmail.com>
2017-02-26 20:39:32 -0800

    phase-2: Add non-privleged system user, clone repo, and install.

--- install-api-env.sh
+++ install-api-env.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+dependencies=(git python3 python3-dev python3-setuptools)
+proc_serve_user="procserve"
+install_dir="/opt"
 
 #Update package repos and install dependencies
 apt-get update
-dependencies=(git python3 python3-dev python3-setuptools)
 for dep in "${dependencies[@]}"
 do
     apt-get install -y $dep
 done
+
+#Add procserve system user
+useradd -r $proc_serve_user
+
+#clone repo to opt
+cd ${install_dir}
+git clone https://github.com/Josh-Willhite/proc-serve.git
+
+#install proc-serve
+cd ${install_dir}/proc-serve
+python3 setup.py install
+chmod 755 src/proc_serve.py
